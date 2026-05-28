053276e0d941b4a74d87992a0c976fe998faff90
Ferdi Tuinman <tuinmanferdi@gmail.com>
2026-05-28 22:56:26 +0200

    deploy do 28-05-2026 22:56:26,41

--- header.php
+++ header.php
@@ -102,19 +102,3 @@ $lnka = "font-['Public_Sans'] text-lg font-bold tracking-tight text-[#078930] un
      href="<?php echo $contact_url; ?>">Contact</a>
 </div>
 </header>
-<script>
-(function(){
-  var wrap = document.querySelector('.nav-dropdown');
-  var btn = wrap && wrap.querySelector('button');
-  if(!btn) return;
-  btn.addEventListener('click', function(e){
-    e.stopPropagation();
-    var open = wrap.classList.toggle('open');
-    btn.setAttribute('aria-expanded', open);
-  });
-  document.addEventListener('click', function(){
-    wrap.classList.remove('open');
-    btn.setAttribute('aria-expanded', 'false');
-  });
-})();
-</script>
